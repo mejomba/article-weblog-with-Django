d06bd9067bcfa88178cb15f1018334cd4778f3b9
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2020-05-08 19:51:12 +0430

    add post ID to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -11,7 +11,7 @@ admin.site.register(UserProfile, AdminUserProfile)
 
 class AdminArticle(admin.ModelAdmin):
     search_fields = ['title', 'content']
-    list_display = ['title', 'cover', 'promote', 'category', 'author', 'created_at']
+    list_display = ['title', 'cover', 'promote', 'category', 'author', 'created_at', 'id']
 
 
 admin.site.register(Article, AdminArticle)
